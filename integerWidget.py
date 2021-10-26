8e4cd846f1e2c0c0bb655e4e39ac72b1214033ef
Dru Staltman <dstaltman@gmail.com>
2021-10-26 08:03:58 -0700

    Integer widget and tests

---
+++ integerWidget.py
@@ -0,0 +1,137 @@
+import sys
+
+import pydash
+from PySide6.QtCore import Slot
+from PySide6.QtGui import QIntValidator
+from PySide6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QApplication
+
+
+# Widget that will control integer values in a json blob.
+#
+# Can control the integer with a text box or buttons to increment and decrement.
+class IntegerWidget(QWidget):
+    def __init__(self, label: str, json_blob: dict, json_location: str):
+        super().__init__()
+        self.label = label
+        self.jsonBlob = json_blob
+        self.jsonLocation = json_location
+
+        layout = QVBoxLayout()
+
+        self.label = QLabel(label)
+        layout.addWidget(self.label)
+
+        # Number and buttons row
+        row = QHBoxLayout()
+
+        # Button with -5
+        self.minusFiveBtn = QPushButton("-5")
+        self.minusFiveBtn.clicked.connect(self.minus_five_pressed)
+        row.addWidget(self.minusFiveBtn)
+
+        # Button with -1
+        self.minusOneBtn = QPushButton("-1")
+        self.minusOneBtn.clicked.connect(self.minus_one_pressed)
+        row.addWidget(self.minusOneBtn)
+
+        # The Text box with the integer
+        self.textBox = QLineEdit("")
+        self.textBox.textEdited.connect(self.text_changed)
+        validator = QIntValidator(0, 100, self)
+        self.textBox.setValidator(validator)
+        row.addWidget(self.textBox)
+
+        # Button with +1
+        self.addOneBtn = QPushButton("+1")
+        self.addOneBtn.clicked.connect(self.add_one_pressed)
+        row.addWidget(self.addOneBtn)
+
+        # Button with +5
+        self.addFiveBtn = QPushButton("+5")
+        self.addFiveBtn.clicked.connect(self.add_five_pressed)
+        row.addWidget(self.addFiveBtn)
+
+        layout.addLayout(row)
+
+        self.setLayout(layout)
+
+        # Get initial value
+        self.read_value()
+
+    def read_value(self):
+        val = pydash.get(self.jsonBlob, self.jsonLocation)
+
+        # No matter the dataType we get input, output as a string
+        # We will error if it is not int or empty though
+        # This protects us from dict or array
+        setJsonValue = False
+        if val is None:
+            val = "0"
+            setJsonValue = True
+        elif type(val) is int:
+            val = str(val)
+        else:
+            raise TypeError('Expected no value, int, or string')
+
+        self.textBox.setText(val)
+        if setJsonValue:
+            self.set_json_value()
+
+    def change_value(self, add: int):
+        # Convert to int. This will not catch exceptions
+        # as we are relying on the TextLine validator
+        # truth is the text line value
+        val = int(self.textBox.text())
+        val += add
+        self.textBox.setText(str(val))
+        self.set_json_value()
+
+    def set_json_value(self):
+        # Convert to int. This will not catch exceptions
+        # as we are relying on the TextLine validator
+        # truth is the text line value
+        val = int(self.textBox.text())
+        pydash.set_(self.jsonBlob, self.jsonLocation, val)
+
+    @Slot()
+    def minus_one_pressed(self):
+        self.change_value(-1)
+
+    @Slot()
+    def minus_five_pressed(self):
+        self.change_value(-5)
+
+    @Slot()
+    def add_five_pressed(self):
+        self.change_value(5)
+
+    @Slot()
+    def add_one_pressed(self):
+        self.change_value(1)
+
+    @Slot()
+    def text_changed(self, text):
+        # Convert to int. This will not catch exceptions
+        # as we are relying on the TextLine validator
+        if len(text) > 0:
+            val = int(text)
+        else:
+            val = 0
+
+        pydash.set_(self.jsonBlob, self.jsonLocation, val)
+
+
+if __name__ == "__main__":
+    # QT application
+    app = QApplication(sys.argv)
+
+    # test json data
+    data = {"index": 10}
+
+    # Widget
+    window = IntegerWidget("Integer", data, 'index')
+
+    window.resize(800, 600)
+    window.show()
+
+    sys.exit(app.exec())
---
+++ test_integerWidget.py
@@ -0,0 +1,83 @@
+import unittest
+from integerWidget import IntegerWidget
+from PySide6.QtTest import QTest
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import Qt
+
+
+class IntegerWidgetTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        super(IntegerWidgetTestCase, self).setUp()
+        if isinstance(QApplication.instance(), type(None)):
+            self.app = QApplication()
+        else:
+            self.app = QApplication.instance()
+
+    def tearDown(self) -> None:
+        del self.app
+        return super(IntegerWidgetTestCase, self).tearDown()
+
+    def testButtons(self):
+        data = {"index": 10}
+        widget = IntegerWidget("integer", data, "index")
+        self.assertEqual("integer", widget.label.text())
+
+        QTest.mouseClick(widget.minusFiveBtn, Qt.LeftButton)
+        self.assertEqual("5", widget.textBox.text())
+        self.assertEqual(5, widget.jsonBlob["index"])
+
+        QTest.mouseClick(widget.minusOneBtn, Qt.LeftButton)
+        self.assertEqual(4, widget.jsonBlob["index"])
+
+        QTest.mouseClick(widget.addFiveBtn, Qt.LeftButton)
+        self.assertEqual(9, widget.jsonBlob["index"])
+
+        QTest.mouseClick(widget.addOneBtn, Qt.LeftButton)
+        self.assertEqual(10, widget.jsonBlob["index"])
+
+    def testTextInputs(self):
+        data = {"index": 10}
+        widget = IntegerWidget("integer", data, "index")
+
+        # Nothing changes when numbers entered
+        QTest.keyClicks(widget.textBox, "hello")
+        self.assertEqual("10", widget.textBox.text())
+
+        # clear with backspace then enter a number
+        QTest.keyClicks(widget.textBox, "\b\b5")
+        self.assertEqual("5", widget.textBox.text())
+        self.assertEqual(5, widget.jsonBlob["index"])
+
+        # clear with backspace then enter a negative number
+        # negative will fail
+        QTest.keyClicks(widget.textBox, "\b\b\b-3")
+        self.assertEqual("3", widget.textBox.text())
+        self.assertEqual(3, widget.jsonBlob["index"])
+
+        # clear with backspace then enter a 4 digit number
+        # will only have 3 numbers
+        QTest.keyClicks(widget.textBox, "\b\b\b4444")
+        self.assertEqual("444", widget.textBox.text())
+        self.assertEqual(444, widget.jsonBlob["index"])
+
+    def test_new_value(self):
+        data = {}
+        widget = IntegerWidget("integer", data, "index")
+
+        # empty should create with a 0 value
+        self.assertEqual("0", widget.textBox.text())
+        self.assertEqual(0, widget.jsonBlob["index"])
+
+        # set a number and it should be set in box
+        QTest.keyClicks(widget.textBox, "0010")
+        self.assertEqual("00010", widget.textBox.text())
+        self.assertEqual(10, widget.jsonBlob["index"])
+
+    def test_error_value(self):
+        data = {"index": "string"}
+        with self.assertRaises(TypeError):
+            IntegerWidget("name", data, "index")
+
+
+if __name__ == '__main__':
+    unittest.main()
